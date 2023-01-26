04ec4e0ad770b83561e5c589af0353222e3f9106
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 01:27:14 +0800

    defining function

---
+++ Selection-Sort.py
@@ -0,0 +1,15 @@
+def selectionSort(array, size):
+    for i in range(size):
+        index = i
+
+        for j in range(i + 1, size):
+            if array[j] < array[index]:
+                index = j
+
+        (array[i], array[index]) = (array[index], array[i])
+
+array_list = [79, 74, 28, 90, 49, 93, 62, 72, 52, 22]
+size = len(array_list)
+selectionSort(array_list, size)
+
+print("The sorted array: ", array_list)
