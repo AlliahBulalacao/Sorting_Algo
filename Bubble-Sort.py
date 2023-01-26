35058f6fa992f3a4bde2dc4e4a69217287465e21
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 02:15:16 +0800

    array list of Insertion Sort

--- Bubble-Sort.py
+++ Bubble-Sort.py
@@ -4,11 +4,10 @@ def bubbleSort(array):
     index = len(array)
 
     for i in range(index):
-        for j in (0, index - i - 1):
+        for j in range(0, index - i - 1):
 
-            if array[j] > array [j+1]:
-                array[j], array[j+1] = array[j+1], array[j]
-
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
 
 array = [79, 74, 28, 90, 49, 93, 62, 72, 52, 22]
 bubbleSort(array)
--- Insertion-Sort.py
+++ Insertion-Sort.py
@@ -4,10 +4,17 @@ def insertionSort(array):
     for i in range(1, len(array)):
 
         n = array[i]
-        j = i -1
+        j = i - 1
 
         while j >= 0 and n < array[j]:
             array[j + 1] = array[j]
             j -= 1
 
+        array[j + 1] = n
+
     return array
+
+array = [79, 74, 28, 90, 49, 93, 62, 72, 52, 22]
+insertionSort(array)
+
+print("Sorted array for Insertion Sort: ", array)
