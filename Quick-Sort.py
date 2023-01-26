d34de88f9a84e94b3c2686dd71184911dc13ce09
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 02:36:13 +0800

    definining partition function

--- Quick-Sort.py
+++ Quick-Sort.py
@@ -6,3 +6,22 @@ def quickSort(array, left, right):
         quickSort(array, left, p - 1)
         quickSort(array, p + 1, right)
 
+def partition(array, left, right):
+    g = left
+    h = right - 1
+    pivot = array(right)
+
+    while g < h:
+        while g < right and array[g] < pivot:
+            g += 1
+        while h > left and array[h] >= pivot:
+            h -= 1
+
+        if g < h:
+            array[g], array[h] = array[h], array[g]
+
+    if array[g] > pivot:
+        array[g], array[right] = array[right], array[g]
+
+    return g
+
