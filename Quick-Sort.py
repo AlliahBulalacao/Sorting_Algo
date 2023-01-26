68804659c84b5a0bb2b1dab4460f8d424a06b677
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 02:42:49 +0800

    array list for Quick Sort

--- Quick-Sort.py
+++ Quick-Sort.py
@@ -7,21 +7,25 @@ def quickSort(array, left, right):
         quickSort(array, p + 1, right)
 
 def partition(array, left, right):
-    g = left
-    h = right - 1
-    pivot = array(right)
+    i = left
+    j = right - 1
+    pivot = array[right]
 
-    while g < h:
-        while g < right and array[g] < pivot:
-            g += 1
-        while h > left and array[h] >= pivot:
-            h -= 1
+    while i < j:
+        while i < right and array[i] < pivot:
+            i += 1
+        while j > left and array[j] >= pivot:
+            j -= 1
 
-        if g < h:
-            array[g], array[h] = array[h], array[g]
+        if i < j:
+            array[i], array[j] = array[j], array[i]
 
-    if array[g] > pivot:
-        array[g], array[right] = array[right], array[g]
+    if array[i] > pivot:
+        array[i], array[right] = array[right], array[i]
 
-    return g
+    return i
 
+array = [79, 74, 28, 90, 49, 93, 62, 72, 52, 22]
+quickSort(array, 0, len(array) - 1)
+
+print("Sorted array for Quick Sort: ", array)
