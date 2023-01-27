262837de7932e550bcf2b12bdbe7a69c5fff2d5b
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 09:34:58 +0800

    defining merge sort function

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -7,3 +7,27 @@ def mergeSort(array):
 
         mergeSort(left)
         mergeSort(right)
+
+        i = 0 #left_index
+        j = 0 #right_index
+        k = 0 #merge_index
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+
+            while i < len(left):
+                array[k] = left[i]
+                i += 1
+                k += 1
+
+            while j < len(right):
+                array = right[j]
+                j += 1
+                k += 1
+
