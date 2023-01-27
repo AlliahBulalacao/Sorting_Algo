aed133fca0fbbd758c5c52c6b2693257543fca53
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2023-01-27 21:01:19 +0800

    fixing the code

--- Merge-Sort.py
+++ Merge-Sort.py
@@ -2,11 +2,12 @@
 
 def mergeSort(array):
     if len(array) > 1:
-        left = array[:len(array)//2]
+        left = array[:len(array) // 2]
         right = array[len(array) // 2:]
 
         mergeSort(left)
         mergeSort(right)
+        print(left,right)
 
         i = 0 #left_index
         j = 0 #right_index
@@ -27,7 +28,11 @@ def mergeSort(array):
                 k += 1
 
             while j < len(right):
-                array = right[j]
+                array[k] = right[j]
                 j += 1
                 k += 1
 
+array = [79, 74, 28, 90, 49, 93, 62, 72, 52, 22]
+mergeSort(array)
+
+print("Sorted array for Merge Sort: ", array)
